34b75ea7228f6be02cf28f8277ebcc8946038e2e
Craze88 <atsuprina@gmail.com>
2017-04-15 22:03:47 +0300

    added some stuff

--- Tutorial21/src/ee/mystudy/test/Main.java
+++ Tutorial21/src/ee/mystudy/test/Main.java
@@ -1,10 +1,26 @@
 package ee.mystudy.test;
 
 public class Main {
-
+	private static int number;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Thread t1 = new Thread(new Worker());
+		Thread t2 = new Thread(new Worker());
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		System.out.println(number);
 	}
-
+	public static synchronized void increaceNumber(){
+		number++;
+	}
 }
